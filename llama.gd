6317abc7f1cfb19615a96673a4d86a451a67877a
Adriankhl <adrian.k.h.lai@outlook.com>
2024-04-29 19:47:31 +0800

    Add stop button to stop text generation

--- llama.gd
+++ llama.gd
@@ -1,17 +1,30 @@
 extends GDLlama
 
+
 var thread: Thread = Thread.new()
 
 
-func _thread_generate_text(prompt: String):
-	var gt = generate_text(prompt)
-	print(gt)
+func _ready():
+	## thread becomes inactive after this calls
+	thread.start(func(): return)
 
+
+func _thread_generate_text(prompt: String):
+	var full_generated_text = generate_text(prompt)
+	print(full_generated_text)
+
+
+## Text generation is slow, use a thread to prevent blocking main thread
 func run_generate_text(prompt: String):
+	## Only one extra thread is allowed
 	if (thread.is_alive()):
 		assert(false, "Llama is already generating text")	
 	else:
+		thread.wait_to_finish()
+		thread = Thread.new()
 		thread.start(_thread_generate_text.bind(prompt))
 
+
+## Always cleanup the thread
 func _exit_tree():
 	thread.wait_to_finish()

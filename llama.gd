57ac1f2fff14fb779bdf20bb1893c261fb74b2e0
Adriankhl <adrian.k.h.lai@outlook.com>
2024-04-29 18:13:19 +0800

    Working generation ui

--- llama.gd
+++ llama.gd
@@ -8,8 +8,10 @@ func _thread_generate_text(prompt: String):
 	print(gt)
 
 func run_generate_text(prompt: String):
-	thread.wait_to_finish()
-	thread.start(_thread_generate_text.bind(prompt))
+	if (thread.is_alive()):
+		assert(false, "Llama is already generating text")	
+	else:
+		thread.start(_thread_generate_text.bind(prompt))
 
 func _exit_tree():
 	thread.wait_to_finish()

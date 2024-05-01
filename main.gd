0f24db2e6bdb6f62839d0d187a93f52a027d4311
Adriankhl <adrian.k.h.lai@outlook.com>
2024-05-01 16:22:27 +0800

    Add instruct mode

--- main.gd
+++ main.gd
@@ -4,6 +4,7 @@ extends Node
 func _ready():
 	## Disable the generate button while the thread is running
 	$GenerateButton.disabled = $Llama.thread.is_alive()
+	$ContinueButton.disabled = true
 
 
 func _process(_delta):
@@ -21,5 +22,30 @@ func _on_llama_generate_text_updated(new_text):
 	$GeneratedText.add_text(new_text)
 
 
+func _on_continue_button_pressed():
+	$ContinueButton.disabled = true
+	if ($Prompt.text != ""):
+		$GeneratedText.add_text("\n\nPrompt: ")
+		$GeneratedText.add_text($Prompt.text)
+		$GeneratedText.add_text("\n\nAnswer: ")
+	$Llama.input_text($Prompt.text)
+	$Prompt.clear()
+
+
 func _on_stop_generate_button_pressed():
 	$Llama.stop_generate_text()
+
+
+func _on_llama_input_wait_started():
+	$ContinueButton.disabled = false
+
+
+func _on_option_button_item_selected(index):
+	match index:
+		0:
+			$GenerateButton.text = "Generate"
+			$Llama.instruct = false
+		1:
+			$GenerateButton.text = "Start"
+			$Llama.instructs = true
+	$Prompt.clear()

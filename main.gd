bdbb38ad200dce0c46a6da7b1d3ec02a665a9406
Adriankhl <adrian.k.h.lai@outlook.com>
2024-05-02 13:12:00 +0800

    Add interactive mode

--- main.gd
+++ main.gd
@@ -1,10 +1,15 @@
 extends Node
 
+var default_prompt = "Godot Engine is a"
+
+var default_interactive_text = "User: Hi\nAI: Hello. I am an AI chatbot. Would you like to talk?\nUser: Sure!\nAI: What would you like to talk about?\nUser:"
+
 
 func _ready():
 	## Disable the generate button while the thread is running
 	$GenerateButton.disabled = $Llama.thread.is_alive()
 	$ContinueButton.disabled = true
+	$Prompt.text = default_prompt
 
 
 func _process(_delta):
@@ -16,6 +21,8 @@ func _on_generate_button_pressed():
 	$GenerateButton.disabled = true
 	$GeneratedText.clear()
 	$Llama.run_generate_text($Prompt.text)
+	if ($OptionButton.selected != 0):
+		$Prompt.clear()
 
 
 func _on_llama_generate_text_updated(new_text):
@@ -25,9 +32,13 @@ func _on_llama_generate_text_updated(new_text):
 func _on_continue_button_pressed():
 	$ContinueButton.disabled = true
 	if ($Prompt.text != ""):
-		$GeneratedText.add_text("\n\nPrompt: ")
-		$GeneratedText.add_text($Prompt.text)
-		$GeneratedText.add_text("\n\nAnswer: ")
+		match $OptionButton.selected:
+			1:
+				$GeneratedText.add_text("\n\nPrompt: ")
+				$GeneratedText.add_text($Prompt.text)
+				$GeneratedText.add_text("\n\nAnswer: ")
+			2:
+				$GeneratedText.add_text($Llama.input_prefix + $Prompt.text)
 	$Llama.input_text($Prompt.text)
 	$Prompt.clear()
 
@@ -45,7 +56,21 @@ func _on_option_button_item_selected(index):
 		0:
 			$GenerateButton.text = "Generate"
 			$Llama.instruct = false
+			$Llama.interactive = false
+			$Llama.reverse_prompt = ""
+			$Llama.input_prefix = ""
+			$Prompt.text = default_prompt
 		1:
 			$GenerateButton.text = "Start"
 			$Llama.instruct = true
-	$Prompt.clear()
+			$Llama.interactive = false
+			$Llama.reverse_prompt = ""
+			$Llama.input_prefix = ""
+			$Prompt.clear()
+		2:
+			$GenerateButton.text = "Start"
+			$Llama.instruct = false
+			$Llama.interactive = true
+			$Llama.reverse_prompt = "User:"
+			$Llama.input_prefix = " "
+			$Prompt.text = default_interactive_text
